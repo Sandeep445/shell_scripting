1133f92535a4bd44bcbc1c174d69fe875e53d2ff
Sandeep445 <sandeepranabothu@gmail.com>
2022-01-09 11:08:32 +0530

    Practice

--- file2.sh
+++ file2.sh
@@ -3,3 +3,5 @@
 export PS1=[ \[\e[1;31m\]\u\[\e[m\]@\[\e[1;33m\]fronted\[\e[m\] \[\e[1;36m\]\w\[\e[m\] ]\$
 
 export PS1=[\e[1;31m\]\u\[\e[m\]@\[\e[1;33m\]fronted\[\e[m\] \[\e[1;36m\]\w\[\e[m\] ]\$
+
+export PS1="[\e1;31m$(whoami)@[\e1;33m$(hostname):[\e1;36m$(pwd) >"
