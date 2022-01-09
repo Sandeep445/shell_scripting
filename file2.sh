67759e8dd955250291a3080915b4fb0d0707690a
Sandeep445 <sandeepranabothu@gmail.com>
2022-01-09 10:59:02 +0530

    Practice

--- file2.sh
+++ file2.sh
@@ -2,4 +2,4 @@
 
 export PS1=[ \[\e[1;31m\]\u\[\e[m\]@\[\e[1;33m\]fronted\[\e[m\] \[\e[1;36m\]\w\[\e[m\] ]\$
 
-PS1=[\e[1;31m\]\u\[\e[m\]@\[\e[1;33m\]fronted\[\e[m\] \[\e[1;36m\]\w\[\e[m\] ]\$
+export PS1=[\e[1;31m\]\u\[\e[m\]@\[\e[1;33m\]fronted\[\e[m\] \[\e[1;36m\]\w\[\e[m\] ]\$
